fdcdbbd6d0d344ff6ded821fc098a2e7abf99da9
cfabian <cfabian@student.42wolfsburg.de>
2021-05-14 12:44:27 +0200

    fd-functions

---
+++ ft_putendl_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/14 12:39:20 by cfabian           #+#    #+#             */
+/*   Updated: 2021/05/14 12:40:13 by cfabian          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len] != 0)
+		len++;
+	return (len);
+}
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	write(fd, &s, ft_strlen(s));
+	write(fd, '\n', 1);
+}
---
+++ ft_putstr_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/14 12:37:08 by cfabian           #+#    #+#             */
+/*   Updated: 2021/05/14 12:38:41 by cfabian          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len] != 0)
+		len++;
+	return (len);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	write(fd, &s, ft_strlen(s));
+}
