1cb00b365c234ade12bfee9a98fc9044d95b5acc
cfabian <cfabian@student.42wolfsburg.de>
2021-05-27 20:50:27 +0200

    ein träumchen

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,14 +6,27 @@
 /*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 12:19:56 by cfabian           #+#    #+#             */
-/*   Updated: 2021/05/26 18:16:39 by cfabian          ###   ########.fr       */
+/*   Updated: 2021/05/27 20:49:15 by cfabian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* s1 > s2 (first unmatching ASCI value)->return diff,only first n bytes */
-#include <libft.h>
+#include <unistd.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	return (ft_memcmp(s1, s2, n));
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*s1 != *s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+		if (*s1 == 0)
+			return (0);
+		i++;
+		s1++;
+		s2++;
+	}
+	return (0);
 }
