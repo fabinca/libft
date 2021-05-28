acee5460151d4b4b3159b5ee49a39e928fc86acf
cfabian <cfabian@student.42wolfsburg.de>
2021-05-28 12:26:33 +0200

    free lst

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 14:51:52 by cfabian           #+#    #+#             */
-/*   Updated: 2021/05/20 15:09:48 by cfabian          ###   ########.fr       */
+/*   Updated: 2021/05/28 12:23:31 by cfabian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	del(lst->content);
-	free(lst->content);
+	free(lst);
 }
